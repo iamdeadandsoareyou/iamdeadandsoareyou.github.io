5bac21d19a944c433658db4010e74b10ce29db9d
jay <iamdeadandsoareyou1@gmail.com>
2025-02-16 20:35:06 -0600

    added media breaks

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
                     </div>
                 </div>
 
-                <h5>
+                <h5 className={"top-image"}>
                     [built by swim]
                 </h5>
 
